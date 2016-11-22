a192eeca91996f8d18d44ae52016b7f1b60f36dd
bnewman <Feb252016>
2016-11-22 12:22:40 -0500

    Update to user model

--- server/models/users.js
+++ server/models/users.js
@@ -4,6 +4,7 @@ const userModel = mongoose.model('user', new mongoose.Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   email: { type: String, index: true },
   username: { type: String, required: true },
+  password: { type: String, required: true },
   firstName: { type: String },
   lastName: { type: String },
   permissions: {
